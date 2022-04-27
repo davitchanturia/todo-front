fd68f851f3afa1096f27708181fcd29e26bded2e
dato chanturia <datochanturia@redberry.ge>
2022-04-27 20:24:44 +0400

    chore: fetching data on favourite todos page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Grid from 'components/notes/Grid';
+import sendRequest from 'lib/sendRequest';
 
-const Home = () => {
+const Home = (props) => {
   return (
     <div>
       <div className='w-full max-w-7xl mx-auto mt-11'>
         <p>Here you can see your the most important todos.</p>
-        <Grid />
+        <Grid data={props.importantTodos} />
       </div>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  const data = await sendRequest(`/api/todos/importants`);
+
+  return {
+    props: {
+      importantTodos: data.importantTodos,
+    },
+    revalidate: 10
+  }
 };
 
 export default Home;
